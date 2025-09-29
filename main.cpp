7248aa45e6802a35db0dacd219296f0e4ea34c28
fernandomdsanastacio-coder <fernando.m.d.s.anastacio@gmail.com>
2025-09-29 10:31:31 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+
+int main()
+{
+    std::cout<<"Os alunos desta turna sao horriveis em programacao";
+    std::cout<<":)";
+    
+    return 0;
+}
