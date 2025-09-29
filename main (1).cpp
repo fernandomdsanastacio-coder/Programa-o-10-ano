9f014e0611ea250f9e9b8cb6efca4c319f1f2f6c
fernandomdsanastacio-coder <fernando.m.d.s.anastacio@gmail.com>
2025-09-29 10:51:46 +0100

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+int main(){
+    int p1,p2,p3,p4,media;
+    std::cout <<"Por favor, escreva um número";
+    std::cin >>p1;
+    std::cout <<"Por favor, escrevca um número";
+    std::cin >>p2;
+    std::cout <<"Por favor, escreva um número";
+    std::cin >>p3;
+    std::cout <<"Por favor, escreva um número";
+    std::cin >>p4;
+    media = (p1+p2+p3+p4)/4;
+    std::cout <<"A media é "<< media << ".";
+}
