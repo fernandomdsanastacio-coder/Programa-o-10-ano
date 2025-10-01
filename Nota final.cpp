931b590c6dc10ebb576d4d74111812ab9bc2a786
fernandomdsanastacio-coder <fernando.m.d.s.anastacio@gmail.com>
2025-10-01 11:46:50 +0100

    Add files via upload

---
+++ Nota final.cpp
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+int main()
+{
+    float P1;
+    std::cout<<"dá-me a nota ";
+    std::cin>>P1;
+    if(P1>20 || P1<0)
+    {
+        std::cout<<"erro";
+    }
+     else
+     {
+         if(P1>=9.5)
+        {
+            std::cout<<"Passas-te";
+        }
+        else
+        {
+            std::cout<<"Passas-te";
+        }
+    
+         
+     }
+        
+    
+    return 0;
+}
