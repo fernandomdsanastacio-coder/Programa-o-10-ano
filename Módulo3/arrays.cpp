af3cb2d8e34b23a6163b5df549915a4178b3d2e8
Spark_zero <fernando.m.d.s.anastacio@gmail.com>
2025-11-27 14:12:31 +0000

    Refactor arrays.cpp to handle student grades

--- Módulo3/arrays.cpp
+++ Módulo3/arrays.cpp
@@ -2,44 +2,61 @@
 #include <string>
 #include <locale.h>
 #include <cstdlib>
-#include <time.h>
 using namespace std;
 
 int main()
 {
 	setlocale(LC_ALL, "Portuguese");
-	int matriz1[3][5], matriz2[3][5],j,i;
-	for ( int i = 0; i < 3; i++)
+	srand(time(0));
+	int turma1[10][10],turmaM[10][10];
+	int resposta, i, j,c=0;
+	char scl = 'n';
+	string disciplinas[10] = { "Portugues","Ingles","Fisica","TLP","TIC","Matemática","ACS","Filosofia","Quimica","Ed.Fis." }, nomes[10] = { "António","Anabela","Beatriz","Bernardo","Clara","Carlos","Diana","Diogo","Elisabete","Eurico" };
+	cout << "Dá-me as notas da turma\n";
+	for ( i = 0; i < 10; i++) 
 	{
-		for (int j = 0; j < 5; j++)
+		
+		cout << "Nota do/da " << nomes[i] << " : ";
+		for ( j = 0; j < 10; j++)
+		{	
+			cout << disciplinas[j] << "\n";
+			cin >> turma1[i][j];
+		}
+		system("cls");
+	}
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
 		{
-			cout << "num[" << i + 1 << "][" << j + 1 << "]";
-			cin >> matriz1[i][j];
-
-
+			turmaM[i][j] = turma1[i][j]/10;
+		}
+		system("cls");
+	}
+	system("cls");
+	for (i = 0; i < 10; i++)
+	{
+		cout << nomes[i] << " Tem " << turmaM[i][i] << " em " << disciplinas[i] << endl;
+	}
+	cout << "Queres saber qual é a melhor nota? (s/n)";
+	cin >> scl;
+	
+	if (scl == 's')
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (i = 0; i < 10; i++)
+			{
+				c++;
+				if (turmaM[i][j] > turmaM[i+1][j+1])
+				{
+					cout << "O aluno com a maior média é " << nomes[c] << "na disciplina "<<disciplinas[c];
+				}
+			}
 		}
 	}
-	for (int i = 0; i < 3; i++)
+	else
 	{
-		for (int j = 0; j < 5; j++)
-		{
-			
-			cout << matriz1[i][j]<<endl;
 
-
-		}
+		return 0;
 	}
-	
-	
-
-
-	
-	
-	
-	
-
-
-
-
-
 }
