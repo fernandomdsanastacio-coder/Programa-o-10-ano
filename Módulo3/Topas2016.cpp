3f614c7675589a20e5747c995bb548bf0af71cbb
Spark_zero <fernando.m.d.s.anastacio@gmail.com>
2025-12-09 11:41:04 +0000

    Add files via upload

---
+++ Módulo3/Topas2016.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <locale.h>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+	char n[1000];
+
+	int i=0;
+
+	do {
+		i++;
+		cin >> n[i];
+		if (n[i] = 1, 2, 3, 4, 5, 6, 7, 8, 9)
+		{
+
+		}
+	} while (n[i] != '.');
+	
+
+	return 0;
+}
